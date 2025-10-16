1bd122029307a70fd0ded6bf7ed3cc81beeb449b
teja143kt <tejakutchula@gmail.com>
2025-10-16 22:55:52 +0530

    home

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,22 @@ import React from 'react'
 function Header() {
   return (
     <div>
-       {/* <!-- Navbar --> */}
-  <nav class="navbar navbar-expand-lg fixed-top">
-    <div class="container">
-      <a class="navbar-brand text-white" href="#home">Teja Kutchula</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
-          <li class="nav-item"><a class="nav-link" href="#about">About Me</a></li>
-          <li class="nav-item"><a class="nav-link" href="#education">Education</a></li>
-          <li class="nav-item"><a class="nav-link" href="#skills">Skills</a></li>
-          <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
-          <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+      {/* <!-- Navbar --> */}
+      <nav classNameName="navbar navbar-expand-lg fixed-top glass-navbar" id='navbar'>
+        <div classNameName="container ">
+          <div className="collapse navbar-collapse " id="navbarNav">
+            <ul className="navbar-nav ms-auto ">
+              <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
+              <li className="nav-item"><a className="nav-link" href="#about">About Me</a></li>
+               <li className="nav-item"><a className="nav-link" href="#skills">Services</a></li>
+              <a classNameName="navbar-brand  " href="#home" id='title'>Teja Kutchula</a>
+              <li className="nav-item"><a className="nav-link" href="#projects">Portfolio</a></li>
+               <li className="nav-item"><a className="nav-link" href="#skills">Albums</a></li>
+              <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
     </div>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        
+      <section id="home">
+        <div className='para'>
+          <p>your dream wedding awaits</p>
+          <p>Artistic wedding photography</p>
+        </div>
+      </section>
+
     </div>
   )
 }
