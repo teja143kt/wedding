d65a3a8e1f6ae97b4d7ba890c36d44f53752e689
teja143kt <tejakutchula@gmail.com>
2025-10-17 20:32:08 +0530

    bg

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,19 @@ function Home() {
   return (
     <div>
       <section id="home">
-        <div className='para'>
-          <p>your dream wedding awaits</p>
+        <div className='para' id='bg'>
+          <p id='p1'>your dream wedding awaits</p>
           <p>Artistic wedding photography</p>
         </div>
+        <>
+          <p className='p2'>AWARD WINNING PHOTOGRAPHY</p>
+          <p className='p3'>Showcasing your big day in a memorable and unforgettable way.</p>
+          <img src="https://i.pinimg.com/474x/cb/f9/58/cbf958a59f2950ad7bc211ce50961166.jpg" alt="Teja Kutchula" />
+          <p className='p2'>HELLO , MY NAME IS TEJA</p>
+          <p className='p4'>a wedding photographer passionate about capturing love stories <br /> in their most natural and beautiful moments.
+            I believe every wedding is unique,<br /> and my goal is to create timeless images <br />that reflect genuine emotions, laughter, and the little details <br />that make your day truly yours. With a blend of candid and creative photography,<br /> I strive to turn your special moments into lasting memories.</p>
+        </>
       </section>
-
     </div>
   )
 }
