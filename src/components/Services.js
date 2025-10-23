bfc1c5396e5a510cb79b6d78bd7f55b93ab66b16
teja143kt <tejakutchula@gmail.com>
2025-10-23 23:08:22 +0530

    adding

--- src/components/Services.js
+++ src/components/Services.js
@@ -21,7 +21,7 @@ function Services() {
           {/* Engagement */}
           <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)' }}>
             <div class="" >
-              <img src="https://i.pinimg.com/474x/cb/f9/58/cbf958a59f2950ad7bc211ce50961166.jpg" alt="profile" className=' services' />
+              <img src="https://wallpapers.com/images/hd/traditional-beautiful-engagement-couple-ring-7o6kh4dv11dsf0dp.jpg" alt="profile" className=' services' />
             </div>
             <div >
               <p className='p2' style={{
@@ -33,102 +33,93 @@ function Services() {
                 Our lens turns your special day into timeless memories filled with warmth and emotion.
                 We don’t just take photos — we preserve the feeling of “forever begins here.”
                 Let us frame your first step toward a lifetime of love and togetherness.</p>
-              <p className='fs-3 ' style={{fontFamily: 'Times New Roman',}}>Amount :<a href='http://localhost:3001/contact' style={{textDecoration:'none', color:'rgba(234, 175, 115, 0.9)'}}> ₹25,000</a></p>
-            </div>
-          </div>
-          
-           {/* <!-- puja --> */}
-          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)',background: 'rgba(234, 175, 115, 0.17)' }}>
-            <div class="" >
-              <img src="https://i.pinimg.com/474x/cb/f9/58/cbf958a59f2950ad7bc211ce50961166.jpg" alt="profile" className=' services' />
-            </div>
-            <div >
-              <p className='p2' style={{
-                color: 'rgba(234, 175, 115, 0.9)', fontFamily: 'Times New Roman',
-                fontSize: '40px'
-              }}>Engagement</p>
-              <p className='p4 paragraph'>Love begins with a promise, and we capture that promise in every frame.
-                From shy smiles to joyful laughter, every glance tells your unique story.
-                Our lens turns your special day into timeless memories filled with warmth and emotion.
-                We don’t just take photos — we preserve the feeling of “forever begins here.”
-                Let us frame your first step toward a lifetime of love and togetherness.</p>
-              <p className='fs-3 ' style={{fontFamily: 'Times New Roman',}}>Amount :<a href='http://localhost:3001/contact' style={{textDecoration:'none', color:'rgba(234, 175, 115, 0.9)'}}> ₹25,000</a></p>
+              <p className='fs-3 ' style={{ fontFamily: 'Times New Roman', }}>Amount :<a href='http://localhost:3001/contact' style={{ textDecoration: 'none', color: 'rgba(234, 175, 115, 0.9)' }}> ₹25,000</a></p>
             </div>
           </div>
 
-           {/* <!-- haldi --> */}
-          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)' }}>
+          {/* <!-- puja --> */}
+          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)', background: 'rgba(234, 175, 115, 0.17)' }}>
             <div class="" >
-              <img src="https://i.pinimg.com/474x/cb/f9/58/cbf958a59f2950ad7bc211ce50961166.jpg" alt="profile" className=' services' />
+              <img src="https://vedicbridge.com/wp-content/uploads/2025/08/Marriage-puja.jpg" alt="profile" className=' services' />
             </div>
             <div >
               <p className='p2' style={{
                 color: 'rgba(234, 175, 115, 0.9)', fontFamily: 'Times New Roman',
                 fontSize: '40px'
-              }}>Engagement</p>
-              <p className='p4 paragraph'>Love begins with a promise, and we capture that promise in every frame.
-                From shy smiles to joyful laughter, every glance tells your unique story.
-                Our lens turns your special day into timeless memories filled with warmth and emotion.
-                We don’t just take photos — we preserve the feeling of “forever begins here.”
-                Let us frame your first step toward a lifetime of love and togetherness.</p>
-              <p className='fs-3 ' style={{fontFamily: 'Times New Roman',}}>Amount :<a href='http://localhost:3001/contact' style={{textDecoration:'none', color:'rgba(234, 175, 115, 0.9)'}}> ₹25,000</a></p>
+              }}>Puja</p>
+              <p className='p4 paragraph'>Every sacred ritual holds a story of faith, love, and togetherness. We capture the divine glow of every blessing, the soft smiles during prayers, and the heartfelt traditions that begin your wedding journey. Each frame reflects the purity of your bond and the warmth of your family’s love. Let us preserve these timeless moments of devotion beautifully.</p>
+              <p className='fs-3 ' style={{ fontFamily: 'Times New Roman', }}>Amount :<a href='http://localhost:3001/contact' style={{ textDecoration: 'none', color: 'rgba(234, 175, 115, 0.9)' }}> ₹15,000</a></p>
+            </div>
+          </div>
+
+          {/* <!-- haldi --> */}
+          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)' }}>
+            <div class="" >
+              <img src="https://cdn.pixabay.com/photo/2025/01/11/09/08/wedding-9325558_640.jpg" alt="profile" className=' services' />
+            </div>
+            <div >
+              <p className='p2' style={{
+                color: 'rgba(234, 175, 115, 0.9)', fontFamily: 'Times New Roman',
+                fontSize: '40px'
+              }}>Haldi</p>
+              <p className='p4 paragraph'>Golden hues, laughter, and love fill the air as we capture the joy of this vibrant ritual.
+                Every splash of turmeric tells a story of tradition and togetherness.
+                From playful moments to emotional smiles, we freeze the essence of purity and happiness.
+                Our lens celebrates the glow of love, family, and new beginnings.
+                Let every Haldi frame radiate the warmth of your wedding journey.</p>
+              <p className='fs-3 ' style={{ fontFamily: 'Times New Roman', }}>Amount :<a href='http://localhost:3001/contact' style={{ textDecoration: 'none', color: 'rgba(234, 175, 115, 0.9)' }}> ₹20,000</a></p>
             </div>
           </div>
 
           {/* <!-- marriage --> */}
-          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)' }}>
+          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)', background: 'rgba(234, 175, 115, 0.17)' }}>
             <div class="" >
-              <img src="https://i.pinimg.com/474x/cb/f9/58/cbf958a59f2950ad7bc211ce50961166.jpg" alt="profile" className=' services' />
+              <img src="https://i.pinimg.com/originals/e9/ca/ce/e9cacec403a326ed5a0554d0bb5da76d.jpg" alt="profile" className=' services' />
             </div>
             <div >
               <p className='p2' style={{
                 color: 'rgba(234, 175, 115, 0.9)', fontFamily: 'Times New Roman',
                 fontSize: '40px'
-              }}>Engagement</p>
-              <p className='p4 paragraph'>Love begins with a promise, and we capture that promise in every frame.
-                From shy smiles to joyful laughter, every glance tells your unique story.
-                Our lens turns your special day into timeless memories filled with warmth and emotion.
-                We don’t just take photos — we preserve the feeling of “forever begins here.”
-                Let us frame your first step toward a lifetime of love and togetherness.</p>
-              <p className='fs-3 ' style={{fontFamily: 'Times New Roman',}}>Amount :<a href='http://localhost:3001/contact' style={{textDecoration:'none', color:'rgba(234, 175, 115, 0.9)'}}> ₹25,000</a></p>
+              }}>Marriage</p>
+              <p className='p4 paragraph'>A wedding is not just a day — it’s a story of love, emotion, and unforgettable moments. We capture every smile, tear, and touch that defines your special day. Each photograph reflects the beauty of your bond and the joy of togetherness. Our passion is to turn your wedding memories into timeless art. Let us frame your love story — beautifully and forever.</p>
+              <p className='fs-3 ' style={{ fontFamily: 'Times New Roman', }}>Amount :<a href='http://localhost:3001/contact' style={{ textDecoration: 'none', color: 'rgba(234, 175, 115, 0.9)' }}> ₹35,000</a></p>
             </div>
           </div>
 
           {/* <!-- Griha Pravesh --> */}
           <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)' }}>
             <div class="" >
-              <img src="https://i.pinimg.com/474x/cb/f9/58/cbf958a59f2950ad7bc211ce50961166.jpg" alt="profile" className=' services' />
+              <img src="https://www.utsavpedia.com/wp-content/uploads/2014/10/entering-the-new-house.jpg" alt="profile" className=' services' />
             </div>
             <div >
               <p className='p2' style={{
                 color: 'rgba(234, 175, 115, 0.9)', fontFamily: 'Times New Roman',
                 fontSize: '40px'
-              }}>Engagement</p>
-              <p className='p4 paragraph'>Love begins with a promise, and we capture that promise in every frame.
-                From shy smiles to joyful laughter, every glance tells your unique story.
-                Our lens turns your special day into timeless memories filled with warmth and emotion.
-                We don’t just take photos — we preserve the feeling of “forever begins here.”
-                Let us frame your first step toward a lifetime of love and togetherness.</p>
-              <p className='fs-3 ' style={{fontFamily: 'Times New Roman',}}>Amount :<a href='http://localhost:3001/contact' style={{textDecoration:'none', color:'rgba(234, 175, 115, 0.9)'}}> ₹25,000</a></p>
+              }}>Griha Pravesh</p>
+              <p className='p4 paragraph'>Stepping into a new home marks the beginning of countless cherished memories.
+                We capture the warmth, traditions, and smiles that fill every corner of your special day.
+                From sacred rituals to joyful laughter, every frame tells the story of new beginnings.
+                Let us preserve the love and blessings that make your Griha Pravesh truly unforgettable.</p>
+              <p className='fs-3 ' style={{ fontFamily: 'Times New Roman', }}>Amount :<a href='http://localhost:3001/contact' style={{ textDecoration: 'none', color: 'rgba(234, 175, 115, 0.9)' }}> ₹15,000</a></p>
             </div>
           </div>
 
           {/* <!-- reception --> */}
-          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)' }}>
+          <div className="col-md-6  pb-3 " id='about' style={{ height: '330px', width: '1100px', borderRadius: '20px', marginLeft: '30px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.68)', background: 'rgba(234, 175, 115, 0.17)' }}>
             <div class="" >
-              <img src="https://i.pinimg.com/474x/cb/f9/58/cbf958a59f2950ad7bc211ce50961166.jpg" alt="profile" className=' services' />
+              <img src="https://i.pinimg.com/736x/d6/1f/b4/d61fb4989cc5ffc25f94734ef2aaa8be.jpg" alt="profile" className=' services' />
             </div>
             <div >
               <p className='p2' style={{
                 color: 'rgba(234, 175, 115, 0.9)', fontFamily: 'Times New Roman',
                 fontSize: '40px'
-              }}>Engagement</p>
-              <p className='p4 paragraph'>Love begins with a promise, and we capture that promise in every frame.
-                From shy smiles to joyful laughter, every glance tells your unique story.
-                Our lens turns your special day into timeless memories filled with warmth and emotion.
-                We don’t just take photos — we preserve the feeling of “forever begins here.”
-                Let us frame your first step toward a lifetime of love and togetherness.</p>
-              <p className='fs-3 ' style={{fontFamily: 'Times New Roman',}}>Amount :<a href='http://localhost:3001/contact' style={{textDecoration:'none', color:'rgba(234, 175, 115, 0.9)'}}> ₹25,000</a></p>
+              }}>Reception</p>
+              <p className='p4 paragraph'>The reception is where joy truly overflows — laughter, dance, and heartfelt emotions come alive.
+                We capture the sparkle of every smile and the rhythm of every celebration.
+                From candid cheers to elegant portraits, each frame reflects the beauty of your union.
+                Our lens tells the story of love, light, and lasting memories that begin anew.
+                Let your grand celebration shine forever through our timeless photographs.</p>
+              <p className='fs-3 ' style={{ fontFamily: 'Times New Roman', }}>Amount :<a href='http://localhost:3001/contact' style={{ textDecoration: 'none', color: 'rgba(234, 175, 115, 0.9)' }}> ₹30,000</a></p>
             </div>
           </div>
         </div>
