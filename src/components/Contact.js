947ca06977436bc4b09d847cefc445bfe4159fc7
teja143kt <tejakutchula@gmail.com>
2025-10-30 22:02:13 +0530

    contactpage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
             color: "rgb(221 128 33 / 90%)",
             fontFamily: "Times New Roman",
             fontSize: "40px",
-            marginTop:'100px'
+            marginTop: "100px",
           }}
         >
           Contact Me
@@ -25,16 +25,16 @@ function Contact() {
           Contact me and let’s begin your story.
         </p>
 
-        {/* Contact Form and Info */}
+        {/* Contact Form and Info Section */}
         <div
-          className="row justify-content-center p-4 rounded shadow"
+          className="row justify-content-center align-items-center p-4 rounded shadow"
           style={{
             background: "rgba(223, 202, 181, 0.58)",
           }}
         >
-          {/* Form Section */}
+          {/* Contact Form */}
           <div className="col-12 col-md-6 mb-4">
-            <div className="p-3 bg-light shadow-sm rounded">
+            <div className="p-4 bg-light shadow-sm rounded">
               <form>
                 <div className="mb-3">
                   <input
@@ -71,48 +71,54 @@ function Contact() {
                   Send Message
                 </button>
               </form>
+              
             </div>
+            <ul className="list-unstyled mb-4">
+                <li className="mb-3">
+                  <strong>Email: </strong>
+                  <a
+                    href="mailto:tejakutchula@gmail.com"
+                    className="text-decoration-none text-dark"
+                  >
+                    tejakutchula@gmail.com
+                  </a>
+                </li>
+
+                <li className="mb-3">
+                  <strong>Phone: </strong>
+                  <a
+                    href="tel:+916302233031"
+                    className="text-decoration-none text-dark"
+                  >
+                    6302233031
+                  </a>
+                </li>
+
+                <li className="mb-3">
+                  <strong>Location: </strong>
+                  <a
+                    href="https://www.google.com/maps/place/Visakhapatnam"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-decoration-none text-dark"
+                  >
+                    Visakhapatnam
+                  </a>
+                </li>
+              </ul>
+
           </div>
 
-          {/* Contact Information */}
-          <div className="col-12 col-md-6 text-center text-md-start">
-            <ul className="list-unstyled">
-              <li className="mb-3">
-                <strong>Email: </strong>
-                <a
-                  href="mailto:tejakutchula@gmail.com"
-                  className="text-decoration-none text-dark"
-                >
-                  tejakutchula@gmail.com
-                </a>
-              </li>
-
-              <li className="mb-3">
-                <strong>Phone: </strong>
-                <a
-                  href="tel:+916302233031"
-                  className="text-decoration-none text-dark"
-                >
-                  6302233031
-                </a>
-              </li>
-
-              <li className="mb-3">
-                <strong>Location: </strong>
-                <a
-                  href="https://www.google.com/maps/place/Visakhapatnam"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-decoration-none text-dark"
-                >
-                  Visakhapatnam
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <div>
-            <img src=""/>
+          {/* Contact Info + Image */}
+          <div className="col-12 col-md-6">
+            <div className="d-flex flex-column align-items-center align-items-md-start text-center text-md-start">
+              
+              <img
+                src="https://i.pinimg.com/736x/29/04/19/290419b2922283a6a44b452fe3ff529b.jpg"
+                alt="Contact"
+                className="img-fluid rounded shadow-sm portfolioimg"
+              />
+            </div>
           </div>
         </div>
       </section>
